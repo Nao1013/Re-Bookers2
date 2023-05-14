b51f7fe59eb170978c4aca1555bb5cd76bc2ab0e
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-05-14 06:01:07 +0000

    [Add]User Error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,50 @@
 class BooksController < ApplicationController
-  
+
   def create
-    
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
-  
+
   def index
-    @books = Book.all
     @book = Book.pluck(:id)
+    @book_new = Book.new
+    @books = Book.all
+    @user = current_user
+    # @user = @book.userで反映されない
   end
 
   def show
-    @book = Book.find([:id])
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = current_user
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
