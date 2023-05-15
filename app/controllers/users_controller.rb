c24b36cc6b5d238cc401848dec657f0d54f00cf4
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-05-15 11:32:23 +0000

    [Add]AccessLimit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:edit, :update]
   
   def index
     @users = User.all
